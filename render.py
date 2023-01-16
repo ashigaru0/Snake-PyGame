a10fbcce149671bb904e38da91be1ff3c7dbef89
EffectMagical <120435264+EffectMagical@users.noreply.github.com>
2023-01-16 15:49:50 +0700

    ready

--- render.py
+++ render.py
@@ -1,12 +1,11 @@
 import pygame
 
-from apple import SpritesClassic
+from sprite import SpritesClassic
 
 
 class Board:
     def __init__(self, size_board=(8, 8), size_cell=100):
         self.size_cell = size_cell
-        self.radius = size_cell // 2
         self.size_screen = (size_cell * size_board[0], size_cell * size_board[1])
         self.size_board = size_board
 
@@ -27,19 +26,18 @@ class Board:
 
     def render(self):
         image = pygame.image.load('backgrounds_img/background1_game.png')
-        image_rect = image.get_rect()
         image = pygame.transform.scale(image, (int(self.size_screen[0] if self.size_screen[0] > 1000 else 1000),
                                                int(self.size_screen[1] if self.size_screen[1] > 1000 else 1000)))
         self.screen.blit(image, (0, 0))
         for h, data in enumerate(self.board):
             for w, color in enumerate(data):
-                x, y = w * self.size_cell + self.radius, \
-                       h * self.size_cell + self.radius
+                x, y = w * self.size_cell, \
+                       h * self.size_cell
 
                 if color == 1:  # тело змейки
-                    self.screen.blit(SpritesClassic(self.radius).snakes_parts_b(), (x - self.radius, y - self.radius))
+                    self.screen.blit(SpritesClassic(self.size_cell).snakes_parts_b(), (x, y))
                 elif color == 2:  # голова змейки
-                    self.screen.blit(SpritesClassic(self.radius).snakes_parts_h(), (x - self.radius, y - self.radius))
-                elif color == 3:  # яблоко (голубика)
-                    self.screen.blit(SpritesClassic(self.radius).changed_image(), (x - self.radius, y - self.radius))
+                    self.screen.blit(SpritesClassic(self.size_cell).snakes_parts_h(), (x, y))
+                elif color == 3:  # голубика
+                    self.screen.blit(SpritesClassic(self.size_cell).changed_image(), (x, y))
         pygame.display.flip()
