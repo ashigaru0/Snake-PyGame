ae754fbd5dae0a67149e1dd53f4fd3de40c772a8
EffectMagical <120435264+EffectMagical@users.noreply.github.com>
2023-01-16 18:58:36 +0700

    Update victory.py

--- victory.py
+++ victory.py
@@ -3,14 +3,6 @@ import pygame
 
 class Victory:
     def __init__(self, text, count):
-        self.text = ['Вы выиграли!', text, f'Ваш результат: {count}']
-
-        self.size_screen = 500, 500
-        self.screen = pygame.display.set_mode(self.size_screen)
-
-
-class Victory:
-    def __init__(self, text, count):
         self.text = ['Вы выиграли!', '', text, '', f'Ваш результат: {count}']
 
         self.size_screen = 500, 500
@@ -53,24 +45,3 @@ class Victory:
                         pygame.display.set_caption('Главное меню')
             self.render()
         pygame.display.set_caption('Победа')
-
-    def render(self):
-        font = pygame.font.Font(None, 40)
-        self.screen.fill((0, 0, 0))
-        text_coord = 20
-        for i, line in enumerate(self.text):
-            string_rendered = font.render(line, True, pygame.Color('white'))
-            text_rect = string_rendered.get_rect()
-            text_coord += 20
-            text_rect.top = text_coord
-            text_rect.x = 20
-            text_coord += text_rect.height
-            self.screen.blit(string_rendered, text_rect)
-        # текст - выход с помощью Enter
-        font_enter = pygame.font.Font(None, 20)
-        text_rendered = font_enter.render('Нажмите Enter для выхода', True, pygame.Color('white'))
-        text_rect = text_rendered.get_rect()
-        text_width, text_height = font_enter.size('Нажмите Enter для выхода')
-        text_rect.x, text_rect.y = 490 - text_width, 490 - text_height
-        self.screen.blit(text_rendered, text_rect)
-        pygame.display.flip()
