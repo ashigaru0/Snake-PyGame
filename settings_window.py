1b74d29a3e10e28fd1249a42a93880e5be64c5a2
EffectMagical <120435264+EffectMagical@users.noreply.github.com>
2023-01-16 16:13:39 +0700

    ready

--- settings_window.py
+++ settings_window.py
@@ -8,9 +8,9 @@ class Settings:
         self.screen = pygame.display.set_mode(self.size_screen)
         self.rules_text = [('Настройки', 75, (10, 10)),
                            ('Проход сквозь себя:', 36, (10, 70), Checkbox(self.screen, 430, 77, 1)),
-                           ('Проход сквозь стены:', 36, (10, 110), Checkbox(self.screen, 430, 117, 2)),
-                           ('Увелечение скорости:', 36, (10, 150), Checkbox(self.screen, 430, 157, 3)),
-                           ('Контроль относительно головы:', 36, (10, 190), Checkbox(self.screen, 430, 197, 4))]
+                           ('Проход сквозь стены:', 36, (10, 100), Checkbox(self.screen, 430, 107, 2)),
+                           ('Увелечение скорости:', 36, (10, 130), Checkbox(self.screen, 430, 137, 3)),
+                           ('Контроль относительно головы:', 36, (10, 160), Checkbox(self.screen, 430, 167, 4))]
         self.load_settings()
 
         pygame.display.set_caption('Настройки')
@@ -21,24 +21,31 @@ class Settings:
             if setting:
                 for indx, line in enumerate(self.rules_text[1::]):
                     self.rules_text[indx + 1][3].checked = False if setting[indx] == 'False' else True
+
     def save_settings(self):
         with open('settings.txt', mode='w') as settings:
             for line in self.rules_text[1::]:
                 settings.write(f'{line[3].checked};')
 
     def render(self):
-        self.screen.fill((0, 0, 0))
-        font = pygame.font.Font(None, self.rules_text[0][1])
-        text = font.render(self.rules_text[0][0], True,
-                           (255, 255, 255)) # 129, 64, 175
-        pygame.draw.rect(self.screen, (61, 0, 153), self.rules_text[0][2] + (270, 50))
-        self.screen.blit(text, self.rules_text[0][2])
-        for line in self.rules_text[1::]:
+        image = pygame.image.load('backgrounds_img/background_settings.png')
+        fon = pygame.transform.scale(image, (500, 500))
+        self.screen.blit(fon, (0, 0))  # фон
+        for indx, line in enumerate(self.rules_text):
             font = pygame.font.Font(None, line[1])
             text = font.render(line[0], True,
                                (255, 255, 255))
             self.screen.blit(text, line[2])
-            line[3].render_checkbox()
+            if indx != 0:
+                line[3].render_checkbox()
+        # текст - выход с помощью Enter
+        font_enter = pygame.font.Font(None, 20)
+        text_rendered = font_enter.render('Нажмите Enter, чтобы вернуться', True, pygame.Color('white'))
+        text_rect = text_rendered.get_rect()
+        text_width, text_height = font_enter.size('Нажмите Enter, чтобы вернуться')
+        text_rect.x, text_rect.y = 490 - text_width, 490 - text_height
+        self.screen.blit(text_rendered, text_rect)
+
         pygame.display.flip()
 
     def running(self):
@@ -50,7 +57,7 @@ class Settings:
                     running = False
                     pygame.display.set_caption('Настройки')
                 elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
+                    if event.key == pygame.K_RETURN:
                         self.save_settings()
                         running = False
                         pygame.display.set_caption('Главное меню')
